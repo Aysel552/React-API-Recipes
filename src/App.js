ed86d73e89c6d98ff51fceed1e3ed6a488b60607
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-23 22:33:44 +0400

    adding a video in the project with css characteristics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect} from 'react';
 import './App.css';
+import video from './recipes.mp4'
 
 function App() {
+  // Adding ID and KEY OF THE API
   const MY_ID = "8925bcff";
   const MY_KEY = "3b7864ef596b2c7c5e52ac133fea723f";
 
+  // adding useEffect hook with API
   useEffect(() => {
     const getRecipe = async () =>{
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=salmon&app_id=${MY_ID}&app_key=${MY_KEY}`);
@@ -15,7 +18,13 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <p>RECIPES</p>
+      <div className="container">
+        <video autoPlay muted loop>
+    <source src={video} type="video/mp4"/>
+        </video>
+
+        <h1>Find a Recipe</h1>
+    </div>
     </div>
   );
 }
