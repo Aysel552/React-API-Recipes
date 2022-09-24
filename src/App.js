9f9a95c568cf8797cf2e39e86ad4abe7facf6593
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-24 19:07:49 +0400

    adding structure of component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect} from 'react';
+import { useEffect, useState} from 'react';
 import './App.css';
 import video from './recipes.mp4'
 
@@ -6,6 +6,9 @@ function App() {
   // Adding ID and KEY OF THE API
   const MY_ID = "8925bcff";
   const MY_KEY = "3b7864ef596b2c7c5e52ac133fea723f";
+
+  // Adding state for search
+  const [mySearch, setMySearch] = useState("");
 
   // adding useEffect hook with API
   useEffect(() => {
@@ -16,14 +19,35 @@ function App() {
     }
     getRecipe();
   }, [])
+
+
+  const myRecipeSearch = (e) => {
+    console.log(e.target.value);
+    setMySearch(e.target.value);
+  }
   return (
+    // Adding video as a background
     <div className="App">
       <div className="container">
         <video autoPlay muted loop>
     <source src={video} type="video/mp4"/>
         </video>
-
         <h1>Find a Recipe</h1>
+    </div>
+  {/* Adding search input */}
+    <div className="container">
+    <form>
+      <input className="search" placeholder="Search..." onChange={myRecipeSearch} value={mySearch}/>
+    </form>
+
+    {/* Adding icon to the button */}
+    <div className="container">
+    <button>
+      <img src="https://img.icons8.com/color/48/000000/fry.png" className="icons" alt="Recipes"/>
+    </button>
+    </div>
+
+
     </div>
     </div>
   );
